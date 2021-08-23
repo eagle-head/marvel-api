3bbc0309e880b48d4c8b8280c66f23e5a59b30a3
eagle-head <edy.kohn@gmail.com>
2021-08-23 05:26:15 -0300

    Add new indentation fo types.d.ts file

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -1,11 +1,7 @@
 declare module "*.png";
-
 declare module "*.jpg";
-
 declare module "*.jpeg";
-
 declare module "*.gif";
-
 declare module "*.svg" {
   const content: string;
   export default content;
